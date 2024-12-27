35744dc00b5cf65b3ee3424014f9384f89e7d4dd
durga shankar pandey <51326341+Durga45@users.noreply.github.com>
2024-12-27 21:17:32 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ element: Component }) => {
-  const token = localStorage.getItem('token');  
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
   if (!token) {
     return <Navigate to="/login" />;
   }
-  return <Component />;
+  return children;
 };
 
 export default ProtectedRoute;
